3e4b86bb7b9fc82c92aee466520c835ae760e6eb
Grupo1 <nvso1@aluno.ifal.edu.br>
2024-08-19 14:26:18 -0300

    inicio do banco

---
+++ banco.java
@@ -0,0 +1,18 @@
+public class banco  {
+
+    String nome = "nathalia";
+    String conta = "n";
+
+    public static void entrar_conta(int nu) {
+        System.out.println(nu);
+        
+    }
+    public static void criar_conta(String nome, double saldo) {
+
+        
+    }
+    public static void transferir (int valor, double conta_origem,double conta_destino)  {
+        
+    }
+
+}
