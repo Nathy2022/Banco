0a72b2cccd219ba76cfb18ae56bbc0ee2900132d
Alisson <161031456+Alisson977@users.noreply.github.com>
2024-08-26 14:20:47 -0300

    Add conta

---
+++ Conta.java
@@ -0,0 +1,19 @@
+public class Conta{
+
+    int numero;
+    String titular;
+    double saldo;   
+    double valor;
+   
+    public void depositar(double valor){
+       valor =saldo;
+       
+    }
+    public static void verificar_saldo(){
+       
+    }
+    public static void nome_do_titular(){
+   
+    }
+   }
+   
